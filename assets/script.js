1d651d2ab01c3660d9835a9d93f0486a3cd4151b
Ross Tauchert <57552771+rdtauche@users.noreply.github.com>
2024-02-11 07:36:25 -0500

    added two javascript projects to site

---
+++ assets/script.js
@@ -0,0 +1,55 @@
+// **********#1 Javascript Code - Timer Countdown**********
+// Selects element by class
+var timeEl = document.querySelector(".time");
+
+// Selects element by id
+var mainEl = document.getElementById("main");
+
+var secondsLeft = 4;
+
+function setTime() {
+  // Sets interval in variable
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
+    timeEl.textContent = secondsLeft + " seconds left till colorsplosion.";
+
+    if(secondsLeft === 0) {
+      // Stops execution of action at set interval
+      clearInterval(timerInterval);
+      // Calls function to create and append image
+      sendMessage();
+    }
+
+  }, 1000);
+}
+
+// Function to create and append colorsplosion image
+function sendMessage() {
+  timeEl.textContent = " ";
+  var imgEl = document.createElement("img");
+  imgEl.setAttribute("src", "./assets/images/image_1.jpg");
+  mainEl.appendChild(imgEl);
+}
+setTime();
+
+// **********#2 Javascript Code - Dark to Light Mode**********
+// Access toggle switch HTML element
+var themeSwitcher = document.querySelector("#theme-switcher");
+var container = document.querySelector(".container");
+
+// Set default mode to dark
+var mode = "dark";
+
+// Listen for a click event on toggle switch
+themeSwitcher.addEventListener("click", function() {
+  // If mode is dark, apply light background
+  if (mode === "dark") {
+    mode = "light";
+    container.setAttribute("class", "light");
+  }
+  // If mode is light, apply dark background 
+  else {
+    mode = "dark";
+    container.setAttribute("class", "dark");
+  }
+});
