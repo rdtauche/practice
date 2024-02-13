aba992d8391b53810e6b01ae76da525e0e5f12f4
Ross Tauchert <57552771+rdtauche@users.noreply.github.com>
2024-02-13 06:47:23 -0500

    added event bubbling javascript example

--- assets/script.js
+++ assets/script.js
@@ -53,3 +53,45 @@ themeSwitcher.addEventListener("click", function() {
     container.setAttribute("class", "dark");
   }
 });
+
+// **************#3 Javascript Code - Contact Me Information********
+var submitEl = document.querySelector("#submit");
+var nameInput = document.querySelector("#name");
+var emailInput = document.querySelector("#email");
+var submissionResponseEl = document.querySelector("#response");
+
+// Action to be performed on click store in named function
+function showResponse(event) {
+  // Prevent default action
+  event.preventDefault();
+  console.log(event);
+  var response = "Thank you for your submission " + nameInput.value + "! We will reach out to you at " + emailInput.value + ".";
+  submissionResponseEl.textContent = response;
+}
+  
+// Add listener to submit element
+submitEl.addEventListener("click", showResponse);
+
+// *************#4 Javascript Code - Event Bubbling Propagation********
+var outer = document.querySelector("#outer-divButton");
+var inner = document.querySelector("#inner-divButton");
+var center = document.querySelector("#center-divButton");
+
+function changeWhite (event) {
+    event.stopPropagation();
+    event.currentTarget.setAttribute("style", "background-color: white");
+}
+
+function changeAquaMarine (event) {
+    event.stopPropagation();
+    event.currentTarget.setAttribute("style", "background-color: aquamarine");
+}
+
+function changePink (event) {
+    event.stopPropagation();
+    event.currentTarget.setAttribute("style", "background-color: pink");
+}
+
+outer.addEventListener("click", changeWhite);
+inner.addEventListener("click", changeAquaMarine);
+center.addEventListener("click", changePink);
